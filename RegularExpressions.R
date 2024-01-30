a0c4fa7169bc5a239416ee88d87ff9bbe2e8ebd5
stephaniejose <bt21994@qmul.ac.uk>
2024-01-30 13:21:20 +0000

    I wrote code which involved the use of the grep() function. Using a piece of text written as a character vector I was able to find the word 'the'. However, in the first try it had returned the words 'the ' and 'they'. To tell R not to return 'they' I used anchors of '^' andd '$'. I was able to find words which contained capital letters, lowercase letters and any letters. Through the use of special characater of '.' I was able to search for words where there is an 'a' and an 'e' and is separated by a character. The function was grep(pattern = 'a.e', x = sentence)

---
+++ RegularExpressions.R
@@ -0,0 +1,34 @@
+#this a vector containing a quote. in the form of a character vector
+sentence <- c('By','the','time','they', 'got', 'back,', 'the', 'lights', 'were', 'all', 'out', 'and', 'everybody', 'was', 'asleep.', 'Everybody,', 'that', 'is,', 'except', 'for', 'Guih', 'Kyom', 'the', 'dung', 'beetle.', 'He', 'was', 'wide', 'awake', 'and', 'on', 'duty,', 'lying', 'on', 'his', 'back', 'with', 'his', 'legs', 'in', 'the', 'air', 'to', 'save', 'the', 'world', 'in', 'case', 'the', 'heavens', 'fell.')
+
+#this is used to find the word 'the' in the text
+grep_out <- grep(pattern = 'the', x = sentence)
+grep_out
+
+
+#this checks if the grep() function has returned all the words and in the correct positions
+#using subscript
+sentence[grep_out]#this included the word 'they', which is not what we wanted 
+
+#the '^' and '$' are known as anchors which will just find 'the' and not anything containing 'the'
+grep_out2 <- grep(pattern = '^the$', x=sentence)
+grep_out2
+
+#regular expressions can be used to search for the elements that contain a capital letter using the pattern '[A-Z]'
+#any letters in the text with a capital letter 
+capital_letter <- grep(pattern = '[A-Z]', x = sentence)
+capital_letter
+
+#we can look for lowercase letters using '[a-z]'
+lowercase_letter <- grep(pattern = '[a-z]', x = sentence)
+lowercase_letter
+
+any_letter <- grep(pattern = '[A-z]', x = sentence)
+any_letter
+
+#we can use the special character '.' to specify that any character can match the search pattern 
+#if we search 'a.e' to look for matches where there is an 'a' and an 'e' separated by any character
+special <- grep(pattern = 'a.e', x = sentence)
+sentence[special]
+
+
